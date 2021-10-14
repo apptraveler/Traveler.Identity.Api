918694f329be0e38828dc04a8c0eeb25270705b4
Gabriel Guedes <gabrielgdc15@gmail.com>
2021-10-14 09:36:05 -0300

    using validations in commands

--- src/Traveler.Identity.Application/Commands/LoginJourneyerCommand.cs
+++ src/Traveler.Identity.Application/Commands/LoginJourneyerCommand.cs
@@ -1,22 +1,24 @@
 using MediatR;
 using Traveler.Identity.Application.DTOs;
+using Traveler.Identity.Application.Validations;
 
 namespace Traveler.Identity.Application.Commands
 {
     public class LoginJourneyerCommand : Command, IRequest<LoginJourneyerResponse>
     {
-        public string UsernameOrEmail { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
 
-        public LoginJourneyerCommand(string usernameOrEmail, string password)
+        public LoginJourneyerCommand(string email, string password)
         {
-            UsernameOrEmail = usernameOrEmail;
+            Email = email;
             Password = password;
         }
 
         public override bool IsValid()
         {
-            return true;
+            var isValidResult = new LoginJourneyerCommandValidation().Validate(this);
+            return isValidResult.IsValid;
         }
     }
 }
