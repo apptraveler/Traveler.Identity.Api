cf4fa20f909539b6078e06f127f08dec772cb6be
Gabriel Guedes <gabrielgdc15@gmail.com>
2021-10-13 22:17:07 -0300

    create migraton for new database changes

---
+++ src/Traveler.Identity.Infra.Data/Migrations/20211014011132_AddNewDatabaseColumnsMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Traveler.Identity.Infra.Data.Migrations
+{
+    public partial class AddNewDatabaseColumnsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "journeyername",
+                table: "journeyer",
+                newName: "username");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isFirstLogin",
+                table: "journeyer",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isFirstLogin",
+                table: "journeyer");
+
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "journeyer",
+                newName: "journeyername");
+        }
+    }
+}
